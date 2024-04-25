fd407162ca170bf1320f8515f4ef83a4865c55e7
asterbot <arjunsodhi7@gmail.com>
2024-04-24 21:07:54 -0400

    fixed some minor inconsistencies

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,13 @@
 import { Box } from '@mui/material';
+
+// Icons used in this page
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import OutlinedFlagRoundedIcon from '@mui/icons-material/OutlinedFlagRounded';
 
+// Timeline components
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -22,12 +25,12 @@ const Home = () => {
 
     // Update all of these as you keep going
     const courses = {
-        "2B": ["CS 241: Foundations of Sequential Programs", "CS 240: Data Structures and Data Management", "MATH 235: Linear Algebra 2 for Honours Mathematics", "PHYS 234: Quantum Physics 1", "FR 151: Basic French 1", "COMMST 225: Interviewing"],
+        "2B": ["CS 241: Foundations of Sequential Programs", "CS 240: Data Structures and Data Management", "MATH 235: Linear Algebra II", "PHYS 234: Quantum Physics I", "FR 151: Basic French I", "COMMST 225: Interviewing"],
         "WT2": ["STAT 231: Statistics"],
         "2A": ["CS 246E: Object-Oriented Software Development (Enriched)", "CS 245: Logic & Computation", "CS 251: Computer Organization and Design", "MATH 249: Introduction to Combinatorics (Advanced Level)", "STAT 230: Probability", "ECON 102: Introduction to Macroeconomics"],
         "WT1": ["CO 250: Introduction to Optimization", "ECON 101: Introduction to Microeconomics"],
         "1B": ["CS 146: Elementary Algorithm Design and Data Abstraction (Advanced Level)", "CS 136L: Tools and Techniques for Software Development", "MATH 136: Linear Algebra I", "MATH 138: Calculus II", "PHYS 122: Waves, Electricity and Magnetism"],
-        "1A": ["CS 145: Designing Functional Programs (Advanced)","MATH 135: Algebra I", "MATH 137: Calculus I", "PHYS 121: Mechanics", "SPCOM 223: Public Speaking"]    
+        "1A": ["CS 145: Designing Functional Programs (Advanced Level)","MATH 135: Algebra I", "MATH 137: Calculus I", "PHYS 121: Mechanics", "SPCOM 223: Public Speaking"]    
     }
 
     const work_term_positions = {
@@ -84,8 +87,9 @@ const Home = () => {
                 className="vertical-timeline-element--work"
                 contentStyle={{ background: '#0e0f0f', color: 'white', border:'2px solid rgb(33, 150, 243)' }}
                 date={timelines[term]}
-                icon={term.slice(0,2)==="WT" ? <WorkOutlineOutlinedIcon /> :  <SchoolOutlinedIcon />}
+                icon={term.slice(0,2)==="WT" ? <WorkOutlineOutlinedIcon /> :  <SchoolOutlinedIcon />} // using the right icon for study/work term
                 >
+                {/* This needed to be hard-coded with slicing but will work in the general sense */}
                 <h3 className="vertical-timeline-element-title">{term.slice(0,2)==="WT" ? "Work Term " + term[2]:  term}</h3>
                 <p className="vertical-timeline-element-subtitle">{term.slice(0,2)==="WT" ? work_term_positions[term] :  "Study Term"}</p>
                 <br />
