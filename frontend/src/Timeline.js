5900dcf8b6e286e2b0a20a49d903af8baa6bac0b
asterbot <arjunsodhi7@gmail.com>
2024-04-29 18:48:01 -0400

    Using split instead of slicing for more reliability

--- frontend/src/Timeline.js
+++ frontend/src/Timeline.js
@@ -52,21 +52,21 @@ const Timeline = () => {
                     className="vertical-timeline-element--work"
                     contentStyle={{ background: '#0e0f0f', color: 'white', border:'2px solid rgb(33, 150, 243)' }}
                     date={timelines[term]}
-                    icon={term.slice(0,2)==="WT" ? <WorkOutlineOutlinedIcon /> :  <SchoolOutlinedIcon />} // using the right icon for study/work term
+                    icon={term.split(" ")[0]==="WT" ? <WorkOutlineOutlinedIcon /> :  <SchoolOutlinedIcon />} // using the right icon for study/work term
                 >
                 {/* This needed to be hard-coded with slicing but will work in the general sense */}
-                <h3 className="vertical-timeline-element-title">{term.slice(0,2)==="WT" ? "Work Term " + term[2] :  term}</h3>
-                <p className="vertical-timeline-element-subtitle">{term.slice(0,2)==="WT" ? work_term_positions[term] :  "Study Term"}</p>
+                <h3 className="vertical-timeline-element-title">{term.split(" ")[0]==="WT" ? "Work Term " + term[2] :  term}</h3>
+                <p className="vertical-timeline-element-subtitle">{term.split(" ")[0]==="WT" ? work_term_positions[term] :  "Study Term"}</p>
                 <br />
                 Courses:
                 <ul className="list-group">
                 {courses[term].map((course)=>{
                     let res = "list-group-item list-group-item-warning";
-                    if (course.slice(0,3)==="CS " || course.slice(0,3)==="CO "){
-                        res="list-group-item list-group-item-" + color_map[course.slice(0,2)];
+                    if (course.split(" ")[0]==="CS" || course.split(" ")[0]==="CO"){
+                        res="list-group-item list-group-item-" + color_map[course.split(" ")[0]];
                     }
-                    else if (course.slice(0,4)==="MATH" || course.slice(0,4)==="PHYS" || course.slice(0,4)==="STAT"){
-                        res="list-group-item list-group-item-" + color_map[course.slice(0,4)]
+                    else if (course.split(" ")[0]==="MATH" || course.split(" ")[0]==="PHYS" || course.split(" ")[0]==="STAT"){
+                        res="list-group-item list-group-item-" + color_map[course.split(" ")[0]]
                     }
                     return (
                         <li className={res} key={course}>{course}</li>
