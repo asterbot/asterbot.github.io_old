928e15d9215546a0bc990325715895c4a68c2513
asterbot <arjunsodhi7@gmail.com>
2024-05-05 13:24:38 -0400

    Minor spacing issue fixed

--- frontend/src/descriptions/music4mood.js
+++ frontend/src/descriptions/music4mood.js
@@ -4,8 +4,7 @@ const Music4Mood = () => {
             This is a <code>Flask</code> web application that uses <a href="https://en.wikipedia.org/wiki/Natural_language_processing">Natural Language Processing</a> to suggest
             personalized songs based on the text entered by the user.
             <br/>
-            The backend of the website is developed in <code>Flask</code> and <code>JavaScript</code> and uses the 
-            <a href="https://developer.spotify.com/documentation/web-api">Spotify Web API</a> to suggest songs based on mood scores. 
+            The backend of the website is developed in <code>Flask</code> and <code>JavaScript</code> and uses the <a href="https://developer.spotify.com/documentation/web-api">Spotify Web API</a> to suggest songs based on mood scores. 
             <br /><br/>
             This was a submission to <a href="https://robohacks2.devpost.com/">RoboHacks 2, 2022</a>
         </div>
